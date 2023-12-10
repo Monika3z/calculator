ab650030c31b48dc19adb8ebcf196dfb96f591e8
monika <monikamonix9@gmail.com>
2023-12-10 17:35:27 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,24 @@
+function appendValue(e){
+    display.value+=e
+
+}
+function clearDisplay(){
+    display.value=eval(display.value)
+
+}
+
+function allClear(){
+    display.value=""
+}
+
+function backSpace(){
+    display.value=display.value.slice(0,-1)
+    
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
